6e0939f43fcb271d4adbfad6ff513486d4fe8c39
tirthpdoshi24 <tirthpdoshi@gmail.com>
2025-08-29 10:24:50 +0530

    Update sqlite_db.py with create table queries

--- sqlite_db.py
+++ sqlite_db.py
@@ -30,6 +30,13 @@ def execute_db(*args):
     db.close()
     return data
 
+#Create category details
+query1 = """CREATE TABLE category_details (id INTEGER PRIMARY KEY, name VARCHAR(255) NOT NULL)"""
+execute_db(query1)
+
+query2 = """CREATE TABLE product_details(id INTEGER PRIMARY KEY,title TEXT NOT NULL,description TEXT NOT NULL,price FLOAT NOT NULL,status
+ INTEGER DEFAULT 1,category_id INTEGER NOT NULL,created_at DATETIME DEFAULT CURRENT_TIMESTAMP,updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,FOREIGN KEY (category_id) REFERENCES category_details(id))"""
+execute_db(query2)
 
 result = execute_db("select pd.*,cd.* from product_details pd left join category_details cd on pd.category_id=cd.id")
 print(result)
